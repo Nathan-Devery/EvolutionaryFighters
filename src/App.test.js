9e6194c67c5953f90e2942c947071b813537e948
nathan <nathandevery@gmail.com>
2021-01-23 16:46:01 +1300

    addd tests again

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import EvolutionUtilities from "./EvolutionUtilities";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// test('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test("generated population size the same", () => {
+  let population = EvolutionUtilities.generatePopulation(10, 10);
+  expect(population.length).toEqual(10);
+  expect(population[0].length).toEqual(10);
 });
+
+test("generated individual correct size", () => {
+  let size = 5;
+  let genome = EvolutionUtilities.generateIndividual(size);
+  expect(genome.length).toEqual(size);
+});
+
+test("generated individual 0 size errors", () => {
+  let size = 0;
+  expect(() => {
+    EvolutionUtilities.generateIndividual(size)
+  }).toThrow();
+});
+
+test("generated individual -1 size errors", () => {
+  let size = -1;
+  expect(() => {
+    EvolutionUtilities.generateIndividual(size)
+  }).toThrow();
+});
+
+test("generated individual 1000 size errors", () => {
+  let size = 1000;
+  expect(() => {
+    EvolutionUtilities.generateIndividual(size)
+  }).toThrow();
+});
+
+
